b68b65213439e38db531ae0c77b7920c56cfd436
Vishal S <98063471+Vishal12s@users.noreply.github.com>
2023-04-15 16:00:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById("btn");
+const shapes = [
+    "quad-circle-1",
+    "quad-circle-2",
+    "quad-circle-3",
+    "quad-circle-4",
+    "triangle-1",
+    "triangle-2",
+    "triangle-3",
+    "triangle-4",
+    "circle"
+    
+];
+
+const colors = [
+    "#d72631","#a2d5c6","#077b8a","#5c3c92"
+]
+const boxes = document.querySelectorAll(".container div");
+let generatePattern = () =>{
+    boxes.forEach((box)=>{
+        box.className = "";
+        let i = Math.floor(Math.random()*shapes.length);
+        let j = Math.floor(Math.random()*colors.length);
+        box.classList.add(shapes[i]);
+        box.style.backgroundColor = colors[j]
+
+    });
+
+};
+btn.addEventListener("click",generatePattern);
+window.addEventListener("load",generatePattern)
